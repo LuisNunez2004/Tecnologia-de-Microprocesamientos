b1d4313e581615d248263de9cb6f508317c62642
TiagoCorrales <tiago.corrales@estudiantes.utec.edu.uy>
2025-09-03 00:15:27 -0300

    Desplazamiento_IzqDer.asm

--- Evaluacion Continua/Matriz de Leds - Tarea Grupal/Desplazamiento_IzqDer.asm
+++ Evaluacion Continua/Matriz de Leds - Tarea Grupal/Desplazamiento_IzqDer.asm
@@ -28,47 +28,50 @@ inicio:
 
        sei
 
-volver:
+Desp_IzqDer:
        sbi PORTB, PORTB7
-       call retardo
+       call retardo1s
        cbi PORTB, PORTB7
-       call retardo
+       call retardo1s
        sbi PORTB, PORTB6
-       call retardo
+       call retardo1s
        cbi PORTB, PORTB6
-       call retardo
+       call retardo1s
        sbi PORTB, PORTB5
-       call retardo
+       call retardo1s
        cbi PORTB, PORTB5
-       call retardo
+       call retardo1s
        sbi PORTB, PORTB4
-       call retardo
+       call retardo1s
        cbi PORTB, PORTB4
-       call retardo
+       call retardo1s
        sbi PORTB, PORTB3
-       call retardo
+       call retardo1s
        cbi PORTB, PORTB3
-       call retardo
+       call retardo1s
        sbi PORTB, PORTB2
-       call retardo
+       call retardo1s
        cbi PORTB, PORTB2
-       call retardo
+       call retardo1s
        sbi PORTB, PORTB1
-       call retardo
+       call retardo1s
        cbi PORTB, PORTB1
-       call retardo
+       call retardo1s
        sbi PORTB, PORTB0
-       call retardo
+       call retardo1s
        cbi PORTB, PORTB0
-       call retardo
+       call retardo1s
 
-retardo:
+       rjmp inicio
+
+retardo1s:
         sbis TIFR1, TOV1
-        rjmp retardo
+        rjmp retardo1s
         sbi TIFR1, TOV1
         ldi r16,  high(34286)
         sts TCNT1H, r16
         ldi r16, low(34286)
         sts TCNT1L, r16
         ret
+
 

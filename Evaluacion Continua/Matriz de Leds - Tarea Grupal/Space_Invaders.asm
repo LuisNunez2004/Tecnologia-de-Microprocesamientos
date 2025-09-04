beb6acdd1fa3084b5422520031dcab06f65ed157
TiagoCorrales <tiago.corrales@estudiantes.utec.edu.uy>
2025-09-04 16:44:01 -0300

    Space_Invaders.asm

--- Evaluacion Continua/Matriz de Leds - Tarea Grupal/Space_Invaders.asm
+++ Evaluacion Continua/Matriz de Leds - Tarea Grupal/Space_Invaders.asm
@@ -10,63 +10,63 @@ inicio:
 
        ldi r16, 0b00111111
        out DDRB, r16
-	   ldi r17, 0b00000011
+	   ldi r17, 0b00001111
 	   out DDRC, r17
-	   ldi r18, 0xFF
+	   ldi r18, 0b11110011
        out DDRD, r18
 
 space_invaders:
 			ldi r16, 0b01111110
-			ldi r17, 0b00000011
-            ldi r18, 0b00100100
+			ldi r17, 0b00000111
+            ldi r18, 0b00100000
 			out PORTB, r16
 			out PORTC, r17
 			out PORTD, r18
             rcall retardo2ms
 
 			ldi r16, 0b01111101
-			ldi r17, 0b00000011
-            ldi r18, 0b01111110
+			ldi r17, 0b00001111
+            ldi r18, 0b01110010
 			out PORTB, r16
 			out PORTC, r17
 			out PORTD, r18
 			rcall retardo2ms
 
 			ldi r16, 0b01111011
-			ldi r17, 0b00000011
-            ldi r18, 0b11011011
+			ldi r17, 0b00001011
+            ldi r18, 0b11010011
 			out PORTB, r16
 			out PORTC, r17
 			out PORTD, r18
 			rcall retardo2ms
 
 			ldi r16, 0b01110111
-			ldi r17, 0b00000011
-            ldi r18, 0b11011011
+			ldi r17, 0b00001011
+            ldi r18, 0b11010011
 			out PORTB, r16
 			out PORTC, r17
 			out PORTD, r18
 			rcall retardo2ms
 
 			ldi r16, 0b01101111
-			ldi r17, 0b00000011
-            ldi r18, 0b11111111
+			ldi r17, 0b00001111
+            ldi r18, 0b11110011
 			out PORTB, r16
 			out PORTC, r17
 			out PORTD, r18
 			rcall retardo2ms
 
 			ldi r16, 0b01011111
-			ldi r17, 0b00000011
-            ldi r18, 0b01011010
+			ldi r17, 0b00001011
+            ldi r18, 0b01010010
 			out PORTB, r16
 			out PORTC, r17
 			out PORTD, r18
 			rcall retardo2ms
 
 			ldi r16, 0b00111111
-			ldi r17, 0b00000010
-            ldi r18, 0b10100101
+			ldi r17, 0b00000110
+            ldi r18, 0b10100001
 			out PORTB, r16
 			out PORTC, r17
 			out PORTD, r18

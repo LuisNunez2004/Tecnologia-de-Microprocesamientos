a1383fbb80564ad4818702218ee09eab9afa35a9
Luis Nunez <luis.nunez@estudiantes.utec.edu.uy>
2025-09-26 14:57:47 -0300

    Subida de informe, evidencia y comentarios

--- Laboratorios/Laboratorio 1/Codigos/Lab1-A.asm
+++ Laboratorios/Laboratorio 1/Codigos/Lab1-A.asm
@@ -1,31 +1,63 @@
-.org    0x0000
-     rjmp inicializacion
-.equ	F_CPU = 16000000
-.equ	baud = 9600
-.equ	bps = (F_CPU/16/baud) - 1
-inicializacion:
-	 ldi r16, high(ramend)
-	 out sph, r16
-	 ldi r16, low(ramend)
-	 out spl, r16
+.org    0x0000             ; Dirección de inicio del programa en la memoria de programa
+     rjmp inicializacion   ; Salta a la rutina de inicialización
+.equ	F_CPU = 16000000    ; Frecuencia del reloj del microcontrolador (16 MHz)
+.equ	baud = 9600         ; Velocidad de transmisión UART (9600 baudios)
+.equ	bps = (F_CPU/16/baud) - 1 ; Valor para configurar el baud rate en UBRR
 
-	 ldi r16, low(bps)
-	 ldi r17, high(bps)
-	 rcall initUART
-	 ldi r16, 0x00
-	 out DDRC, r16
-	 ldi r16, 0x7A
-	 out DDRD, r16
-	 sbi PORTC, 0
-	 rjmp msgInicial
+; ---------------- INICIALIZACIÓN DEL STACK Y PERIFÉRICOS ----------------
+inicializacion:
+	 ldi r16, high(ramend) ; Carga en r16 el byte alto del final de la RAM
+	 out sph, r16          ; Configura el puntero de pila (parte alta)
+	 ldi r16, low(ramend)  ; Carga en r16 el byte bajo del final de la RAM
+	 out spl, r16          ; Configura el puntero de pila (parte baja)
+
+	 ldi r16, low(bps)     ; Carga parte baja del divisor de baudios
+	 ldi r17, high(bps)    ; Carga parte alta del divisor de baudios
+	 rcall initUART        ; Llama a rutina de inicialización UART
+	 ldi r16, 0x00         
+	 out DDRC, r16         ; Puerto C como entrada
+	 ldi r16, 0x7A         
+	 out DDRD, r16         ; Configura DDRD como salida en ciertos pines
+	 sbi PORTC, 0          ; Activa resistencia pull-up en PC0
+	 rjmp msgInicial       ; Salta a enviar mensaje inicial
+
+; ---------------- INICIALIZACIÓN DEL UART ----------------
 initUART:
-   sts UBRR0L, r16
-   sts UBRR0H, r17
-   ldi r16, (1<<RXEN0)|(1<<TXEN0)
-   sts UCSR0B, r16
+   sts UBRR0L, r16         ; Configura parte baja del baud rate
+   sts UBRR0H, r17         ; Configura parte alta del baud rate
+   ldi r16, (1<<RXEN0)|(1<<TXEN0) ; Habilita recepción y transmisión
+   sts UCSR0B, r16         ; Guarda configuración en registro de control
    ret
+
+; ---------------- MENSAJE INICIAL POR UART ----------------
 msgInicial:
-   ldi r16, 0x4C
+   ldi r16, 0x4C           ; "L"
+   rcall putc
+   ldi r16, 0x20           ; " "
+   rcall putc
+   ldi r16, 0x2D           ; "-"
+   rcall putc
+   ldi r16, 0x3E           ; ">"
+   rcall putc
+   ldi r16, 0x20           ; " "
+   rcall putc
+   ldi r16, 0x4C           ; "L"
+   rcall putc
+   ldi r16, 0x69           ; "i"
+   rcall putc
+   ldi r16, 0x67           ; "g"
+   rcall putc
+   ldi r16, 0x65           ; "e"
+   rcall putc
+   ldi r16, 0x72           ; "r"
+   rcall putc
+   ldi r16, 0x61           ; "a"
+   rcall putc
+   ldi r16, 0x0A           ; Salto de línea
+   rcall putc
+
+   ; --- Mensaje "M -> Mediana" ---
+   ldi r16, 0x4D           ; "M"
    rcall putc
    ldi r16, 0x20
    rcall putc
@@ -35,47 +67,25 @@ msgInicial:
    rcall putc
    ldi r16, 0x20
    rcall putc
-   ldi r16, 0x4C
+   ldi r16, 0x4D           ; "M"
    rcall putc
-   ldi r16, 0x69
+   ldi r16, 0x65           ; "e"
    rcall putc
-   ldi r16, 0x67
+   ldi r16, 0x64           ; "d"
    rcall putc
-   ldi r16, 0x65
+   ldi r16, 0x69           ; "i"
    rcall putc
-   ldi r16, 0x72
+   ldi r16, 0x61           ; "a"
    rcall putc
-   ldi r16, 0x61
+   ldi r16, 0x6E           ; "n"
    rcall putc
-   ldi r16, 0x0A
+   ldi r16, 0x61           ; "a"
    rcall putc
-   ldi r16, 0x4D
+   ldi r16, 0x0A           ; Salto de línea
    rcall putc
-   ldi r16, 0x20
-   rcall putc
-   ldi r16, 0x2D
-   rcall putc
-   ldi r16, 0x3E
-   rcall putc
-   ldi r16, 0x20
-   rcall putc
-   ldi r16, 0x4D
-   rcall putc
-   ldi r16, 0x65
-   rcall putc
-   ldi r16, 0x64
-   rcall putc
-   ldi r16, 0x69
-   rcall putc
-   ldi r16, 0x61
-   rcall putc
-   ldi r16, 0x6E
-   rcall putc
-   ldi r16, 0x61
-   rcall putc
-   ldi r16, 0x0A
-   rcall putc
-   ldi r16, 0x50
+
+   ; --- Mensaje "P -> Pesada" ---
+   ldi r16, 0x50           ; "P"
    rcall putc
    ldi r16, 0x20
    rcall putc
@@ -85,45 +95,51 @@ msgInicial:
    rcall putc
    ldi r16, 0x20
    rcall putc
-   ldi r16, 0x50
+   ldi r16, 0x50           ; "P"
    rcall putc
-   ldi r16, 0x65
+   ldi r16, 0x65           ; "e"
    rcall putc
-   ldi r16, 0x73
+   ldi r16, 0x73           ; "s"
    rcall putc
-   ldi r16, 0x61
+   ldi r16, 0x61           ; "a"
    rcall putc
-   ldi r16, 0x64
+   ldi r16, 0x64           ; "d"
    rcall putc
-   ldi r16, 0x61
+   ldi r16, 0x61           ; "a"
    rcall putc
-   rjmp wait
+   rjmp wait               ; Espera entrada del usuario
+
+; ---------------- TRANSMISIÓN UART ----------------
 putc:
-   lds r17, UCSR0A
-   sbrs r17, UDRE0
-   rjmp putc
-   sts UDR0, r16
+   lds r17, UCSR0A         ; Lee estado UART
+   sbrs r17, UDRE0         ; Verifica si buffer está listo
+   rjmp putc               ; Espera hasta que se pueda escribir
+   sts UDR0, r16           ; Envía el dato
    ldi r16, 0
    ret
+
+; ---------------- RECEPCIÓN UART ----------------
 getc:
-   lds r17, UCSR0A
-   sbrs r17, RXC0
-   rjmp getc
-   lds r16, UDR0
+   lds r17, UCSR0A         ; Lee estado UART
+   sbrs r17, RXC0          ; Verifica si hay dato recibido
+   rjmp getc               ; Espera hasta que llegue dato
+   lds r16, UDR0           ; Carga el dato recibido en r16
    ret
+
+; ---------------- PATRÓN "PESADA" ----------------
 pesada:
-	sbi PORTD, 3
-	cbi portd, 5
+	sbi PORTD, 3           ; Enciende PD3
+	cbi portd, 5           ; Apaga PD5
+	rcall s1Delay          ; Delay de 1 segundo
 	rcall s1Delay
 	rcall s1Delay
 	rcall s1Delay
 	rcall s1Delay
-	rcall s1Delay
-	cbi portd, 3
-	rcall s1Delay
+	cbi portd, 3           ; Apaga PD3
 	rcall s1Delay
 	rcall s1Delay
-	sbi portd, 6
+	rcall s1Delay
+	sbi portd, 6           ; Enciende PD6
 	rcall ms200Delay
 	cbi portd, 6
 	rcall s1Delay
@@ -141,15 +157,19 @@ pesada:
 	rcall s1Delay
 	cbi portd, 5
 	rjmp msgInicial
+
+; ---------------- ESPERA DE COMANDOS POR UART ----------------
 wait:
-   rcall getc
-   cpi r16, 0x4C
+   rcall getc              ; Espera caracter por UART
+   cpi r16, 0x4C           ; "L"
    breq ligera
-   cpi r16, 0x4D
+   cpi r16, 0x4D           ; "M"
    breq mediana
-   cpi r16, 0x50
+   cpi r16, 0x50           ; "P"
    breq pesada
    rjmp wait
+
+; ---------------- PATRÓN "LIGERA" ----------------
 ligera:
 	sbi PORTD, 3
 	cbi portd, 5
@@ -173,6 +193,8 @@ ligera:
 	rcall s1Delay
 	cbi portd, 5
 	rjmp msgInicial
+
+; ---------------- DELAY DE 1 SEGUNDO ----------------
 s1Delay:
     ldi r18, 199
 L1: ldi r19, 199
@@ -184,6 +206,8 @@ L3: dec r20
     dec r18
     brne L1
     ret
+
+; ---------------- DELAY DE ~200 ms ----------------
 ms200Delay:
     ldi  r21, 12
     ldi  r22, 255
@@ -195,6 +219,8 @@ m1: dec  r23
     dec  r21
     brne m1
     ret
+
+; ---------------- PATRÓN "MEDIANA" ----------------
 mediana:
 	sbi PORTD, 3
 	cbi portd, 5
@@ -221,4 +247,3 @@ mediana:
 	rcall s1Delay
 	cbi portd, 5
 	rjmp msgInicial
-
--- Laboratorios/Laboratorio 1/Codigos/Lab1-D-E.asm
+++ Laboratorios/Laboratorio 1/Codigos/Lab1-D-E.asm
@@ -1,120 +1,152 @@
-.org    0x0000
-     rjmp inicializacion
-.equ	F_CPU = 16000000
-.equ	baud = 9600
-.equ	bps = (F_CPU/16/baud) - 1
-inicializacion:
-	 ldi r16, high(ramend)
-	 out sph, r16
-	 ldi r16, low(ramend)
-	 out spl, r16
+.org    0x0000             ; Dirección de inicio del programa
+     rjmp inicializacion   ; Salta a la rutina de inicialización
+.equ	F_CPU = 16000000    ; Frecuencia del microcontrolador (16 MHz)
+.equ	baud = 9600         ; Velocidad de transmisión UART
+.equ	bps = (F_CPU/16/baud) - 1 ; Cálculo del divisor para baud rate
 
-	 ldi r16, low(bps)
-	 ldi r17, high(bps)
-	 rcall initUART
-	 ldi r16, 0xFE
-	 out DDRD, r16
-	 rjmp msgInicial
+; ---------------- INICIALIZACIÓN DEL STACK Y UART ----------------
+inicializacion:
+	 ldi r16, high(ramend) ; Carga el byte alto de la RAM
+	 out sph, r16          ; Configura el stack pointer (parte alta)
+	 ldi r16, low(ramend)  ; Carga el byte bajo de la RAM
+	 out spl, r16          ; Configura el stack pointer (parte baja)
+
+	 ldi r16, low(bps)     ; Parte baja del divisor de baudios
+	 ldi r17, high(bps)    ; Parte alta del divisor de baudios
+	 rcall initUART        ; Inicializa UART
+	 ldi r16, 0xFE         
+	 out DDRD, r16         ; Configura puerto D como salida (excepto PD0)
+	 rjmp msgInicial       ; Salta a enviar mensaje inicial
+
+; ---------------- CONFIGURACIÓN UART ----------------
 initUART:
-   sts UBRR0L, r16
-   sts UBRR0H, r17
-   ldi r16, (1<<RXEN0)|(1<<TXEN0)
+   sts UBRR0L, r16         ; Configura parte baja del baud rate
+   sts UBRR0H, r17         ; Configura parte alta del baud rate
+   ldi r16, (1<<RXEN0)|(1<<TXEN0) ; Habilita RX y TX
    sts UCSR0B, r16
    ret
+
+; ---------------- MENSAJE INICIAL ----------------
 msgInicial:
-   ldi r16, 0x31
+   ; "1-Triangulo"
+   ldi r16, 0x31           ; "1"
    rcall putc
-   ldi r16, 0x2D
+   ldi r16, 0x2D           ; "-"
    rcall putc
-   ldi r16, 0x54
+   ldi r16, 0x54           ; "T"
    rcall putc
-   ldi r16, 0x72
+   ldi r16, 0x72           ; "r"
    rcall putc
-   ldi r16, 0x69
+   ldi r16, 0x69           ; "i"
    rcall putc
-   ldi r16, 0x61
+   ldi r16, 0x61           ; "a"
    rcall putc
-   ldi r16, 0x6E
+   ldi r16, 0x6E           ; "n"
    rcall putc
-   ldi r16, 0x67
+   ldi r16, 0x67           ; "g"
    rcall putc
-   ldi r16, 0x75
+   ldi r16, 0x75           ; "u"
    rcall putc
-   ldi r16, 0x6C
+   ldi r16, 0x6C           ; "l"
    rcall putc
-   ldi r16, 0x6F
+   ldi r16, 0x6F           ; "o"
    rcall putc
-   ldi r16, 0x0A
+   ldi r16, 0x0A           ; Salto de línea
    rcall putc
-   ldi r16, 0x32
+
+   ; "2-Circulo"
+   ldi r16, 0x32           ; "2"
    rcall putc
-   ldi r16, 0x2D
+   ldi r16, 0x2D           ; "-"
    rcall putc
-   ldi r16, 0x43
+   ldi r16, 0x43           ; "C"
    rcall putc
-   ldi r16, 0x69
+   ldi r16, 0x69           ; "i"
    rcall putc
-   ldi r16, 0x72
+   ldi r16, 0x72           ; "r"
    rcall putc
-   ldi r16, 0x63
+   ldi r16, 0x63           ; "c"
    rcall putc
-   ldi r16, 0x75
+   ldi r16, 0x75           ; "u"
    rcall putc
-   ldi r16, 0x6C
+   ldi r16, 0x6C           ; "l"
    rcall putc
-   ldi r16, 0x6F
+   ldi r16, 0x6F           ; "o"
    rcall putc
-   ldi r16, 0x0A
+   ldi r16, 0x0A           ; Salto de línea
    rcall putc
-   ldi r16, 0x33
+
+   ; "3-Cruz"
+   ldi r16, 0x33           ; "3"
    rcall putc
-   ldi r16, 0x2D
+   ldi r16, 0x2D           ; "-"
    rcall putc
-   ldi r16, 0x43
+   ldi r16, 0x43           ; "C"
    rcall putc
-   ldi r16, 0x72
+   ldi r16, 0x72           ; "r"
    rcall putc
-   ldi r16, 0x75
+   ldi r16, 0x75           ; "u"
    rcall putc
-   ldi r16, 0x7A
+   ldi r16, 0x7A           ; "z"
    rcall putc
-   ldi r16, 0x0A
+   ldi r16, 0x0A           ; Salto de línea
    rcall putc
-   ldi r16, 0x54
+
+   ; "T-Todo"
+   ldi r16, 0x54           ; "T"
    rcall putc
-   ldi r16, 0x2D
+   ldi r16, 0x2D           ; "-"
    rcall putc
-   ldi r16, 0x54
+   ldi r16, 0x54           ; "T"
    rcall putc
-   ldi r16, 0x6F
+   ldi r16, 0x6F           ; "o"
    rcall putc
-   ldi r16, 0x64
+   ldi r16, 0x64           ; "d"
    rcall putc
-   ldi r16, 0x6F
+   ldi r16, 0x6F           ; "o"
    rcall putc
-   ldi r16, 0x0A
+   ldi r16, 0x0A           ; Salto de línea
    rcall putc
-   rjmp wait
+
+   rjmp wait               ; Salta a la espera de entrada
+
+; ---------------- ENVÍO DE CARÁCTER UART ----------------
 putc:
-   lds r17, UCSR0A
-   sbrs r17, UDRE0
-   rjmp putc
-   sts UDR0, r16
+   lds r17, UCSR0A         ; Lee estado UART
+   sbrs r17, UDRE0         ; Verifica si buffer está listo
+   rjmp putc               ; Si no, espera
+   sts UDR0, r16           ; Envía el dato
    ldi r16, 0
    ret
+
+; ---------------- RECEPCIÓN DE CARÁCTER UART ----------------
 getc:
-   lds r17, UCSR0A
-   sbrs r17, RXC0
-   rjmp getc
-   lds r16, UDR0
+   lds r17, UCSR0A         ; Lee estado UART
+   sbrs r17, RXC0          ; Verifica si hay dato recibido
+   rjmp getc               ; Espera hasta recibirlo
+   lds r16, UDR0           ; Guarda dato recibido en r16
    ret
+
+; ---------------- OPCIÓN "TODO" ----------------
+toda:
+	ldi r22, 1             ; Marca bandera
+	cpi r22, 1
+	breq triangulo         ; Salta a dibujar triángulo primero
+	rjmp msgInicial
+
+; ---------------- ESPERA DE ENTRADA POR UART ----------------
 wait:
-   rcall getc
-   cpi r16, 0x31
+   ldi r22, 0              ; Reinicia bandera
+   rcall getc              ; Espera entrada
+   cpi r16, 0x31           ; "1"
    breq triangulo
-   cpi r16, 0x33
+   cpi r16, 0x33           ; "3"
    breq cruz
+   cpi r16, 0x54           ; "T"
+   breq toda
    rjmp wait
+
+; ---------------- DELAY DE 1 SEGUNDO ----------------
 s1Delay:
     ldi r18, 199
 L1: ldi r19, 199
@@ -126,9 +158,11 @@ L3: dec r20
     dec r18
     brne L1
     ret
+
+; ---------------- FIGURA: TRIÁNGULO ----------------
 triangulo:
 	rcall centrar
-	sbi portd, 2
+	sbi portd, 2           ; Enciende PD2
 	rcall s1delay
 	cbi portd, 2
 	sbi portd, 5
@@ -146,8 +180,42 @@ triangulo:
 	sbi portd, 3
 	rcall s1delay
 	cbi portd, 3
+	cpi r22, 1             ; Si se llamó desde "toda"
+	breq cruzdib           ; Continuar con cruz
 	rcall descentrar
 	rjmp msgInicial
+
+; ---------------- FIGURA: CRUZ ----------------
+cruz:
+	rcall centrar
+cruzdib:
+	sbi portd, 2
+	rcall s1delay
+	cbi portd, 2
+	sbi portd, 4
+	rcall s1delay
+	rcall s1delay
+	cbi portd, 4
+	sbi portd, 5
+	rcall s1delay
+	cbi portd, 5
+	sbi portd, 7
+	rcall s1delay
+	cbi portd, 7
+	sbi portd, 6
+	rcall s1delay
+	rcall s1delay
+	cbi portd, 6
+	sbi portd, 7
+	rcall s1delay
+	cbi portd, 7
+	sbi portd, 3
+	rcall s1delay
+	cbi portd, 3
+	rcall descentrar
+	rjmp msgInicial
+
+; ---------------- EFECTO DE CENTRADO ----------------
 centrar:
 	sbi portd, 7
 	sbi portd, 4
@@ -166,6 +234,8 @@ cenloop2:
 	brne cenloop2
 	cbi portd, 7
 	ret
+
+; ---------------- EFECTO DE DESCENTRADO ----------------
 descentrar:
 	sbi portd, 6
 	sbi portd, 5
@@ -184,29 +254,3 @@ dcenloop2:
 	brne dcenloop2
 	cbi portd, 6
 	ret
-cruz:
-	rcall centrar
-	sbi portd, 2
-	rcall s1delay
-	cbi portd, 2
-	sbi portd, 4
-	rcall s1delay
-	rcall s1delay
-	cbi portd, 4
-	sbi portd, 5
-	rcall s1delay
-	cbi portd, 5
-	sbi portd, 7
-	rcall s1delay
-	cbi portd, 7
-	sbi portd, 6
-	rcall s1delay
-	rcall s1delay
-	cbi portd, 6
-	sbi portd, 7
-	rcall s1delay
-	cbi portd, 7
-	sbi portd, 3
-	rcall s1delay
-	cbi portd, 3
-	rjmp msgInicial
